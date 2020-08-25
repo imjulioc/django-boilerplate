6349d820d49c1ce21aaca746a7da9a7d5e5e3a94
Julio César Alcántara Sánchez <julio.alcantara128@protonmail.com>
2020-08-25 02:16:15 -0500

    Create signup view

--- src/accounts/api/urls.py
+++ src/accounts/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import *
+from .views import UserCreate, login, refresh_token
 
 urlpatterns = [
+    path('signup/', UserCreate.as_view(), name='signup'),
     path('login/', login, name='login'),
-    path('refresh_token/', login, name='refresh_token'),
+    path('refresh_token/', refresh_token, name='refresh_token'),
 ]
--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, status
+from rest_framework import permissions, status, exceptions, generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
@@ -6,7 +6,13 @@ from django.conf import settings
 import jwt
 
 from ..models import User
+from ..serializers import UserCreateSerializer
 from ..utils import generate_access_token, generate_refresh_token
+
+class UserCreate(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = UserCreateSerializer
+    lookup_field = 'id'
 
 @api_view(['POST'])
 @permission_classes([permissions.AllowAny])
@@ -22,8 +28,8 @@ def login(request):
         return Response({'error': 'Username or password is wrong.'}, status=status.HTTP_404_NOT_FOUND)
     response = Response()
 
-    acces_token = generate_access_token(user_profile._id)
-    refresh_token = generate_refresh_token(user_profile._id)
+    acces_token = generate_access_token(user.id)
+    refresh_token = generate_refresh_token(user.id)
 
     response.set_cookie(key='refresh_token', value=refresh_token, httponly=True)
     response.data = {
@@ -52,12 +58,12 @@ def refresh_token(request):
                 'Expired access.'
             )
 
-    user = User.objects.filter(_id=payload['_id']).first()
+    user = User.objects.filter(id=payload['id']).first()
     if user is None:
         raise exceptions.AuthenticationFailed('User not found.')
     if not user.is_active:
             raise exceptions.AuthenticationFailed('User is not active.')
 
-    access_token = generate_access_token(user._id)
+    access_token = generate_access_token(user.id)
 
     return Response({'access_token': access_token})
