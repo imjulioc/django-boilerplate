ebeb39bb304354a9d10a588615901c7acd2c8934
Julio César Alcántara Sánchez <julio.alcantara128@protonmail.com>
2020-08-25 02:15:09 -0500

    Add is_verified field on User model

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 import uuid
 
 class User(AbstractUser):
-    _id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    is_verified = models.BooleanField(default=False)
